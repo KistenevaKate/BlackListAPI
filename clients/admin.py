e9a7411a4515ec1d1a893f266d1c1ddf00929f5b
Kirill Pakhtusov <pak.kiruha@yandex.ru>
2019-11-28 11:07:41 +0300

    Исправлены ошибки в моделях

--- clients/admin.py
+++ clients/admin.py
@@ -1,4 +1,8 @@
+
 from django.contrib import admin
+from .models import Client, Comment_client, Status_client
 
-
+@admin.register(Client, Comment_client, Status_client)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
 # Register your models here.
--- clients/models.py
+++ clients/models.py
@@ -2,31 +2,36 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Clients(models.Model):
-
-    
-    class Meta:
-        verbose_name_plural ="Клиенты"
-        verbose_name = "Клиент"
  
 class Client(models.Model):
     fio = models.CharField(max_length=35)
     telephone = models.CharField(max_length = 15)
     email = models.EmailField(max_length=254) 
-    id_status_client= models.ForeignKey( 'Status_client', on_delete=models.CASCADE, )
+    status_client= models.ForeignKey( 'Status_client', on_delete=models.CASCADE, )
     create_date = models.DateField(auto_now_add=True)
     descriptions = models.CharField(max_length = 500)
 
+    class Meta:
+        verbose_name_plural ="Клиенты"
+        verbose_name = "Клиент"
+
 class Status_client(models.Model):
-	name = models.CharField(max_length = 35)
-	description = model.CharField(max_length = 500)
-	color = models.IntegerField()
+    name = models.CharField(max_length = 35)
+    description = models.CharField(max_length = 500)
+    color = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural ="Статусы клиентов"
+        verbose_name = "Статус клиента"
 
 class Comment_client(models.Model):
-	id_status_client = models.IntegerField()
-	id_client=models.ForeignKey( 'Client', on_delete=models.CASCADE, )
-	id_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	text = models.CharField(max_length = 500)
-	create_date = models.DateField(auto_now_add=True)
+    client=models.ForeignKey( 'Client', on_delete=models.CASCADE, )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.CharField(max_length = 500)
+    create_date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural ="Статусы клиентов"
+        verbose_name = "Статус клиента"
 
 
